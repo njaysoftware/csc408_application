84a2380c2dd65a4c06adc9a0a8c093629c475bde
Nathaniel Padgett <njpadget@geneva.edu>
2019-03-06 20:55:30 -0500

    fixed naming convention on api

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ Route::resource('/customers', 'ApiUserController');
 Route::get('/customers/{id}/rentals', 'ApiUserController@rentals');
 Route::resource('/movies', 'ApiMovieController');
 //returns the data for individuals who rented a single movie
-Route::get('/movies/{id}/rentors', 'ApiMovieController@RentedBy');
+Route::get('/movies/{id}/rentals', 'ApiMovieController@RentedBy');
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
