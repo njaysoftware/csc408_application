bbfc8921537bdeca90b5d7ab9d98414819fa5079
Nathaniel <Nathaniel@Nathaniels-MacBook-Pro.local>
2019-03-25 18:02:13 -0400

    implemented cross origin request functionality

--- routes/api.php
+++ routes/api.php
@@ -12,13 +12,14 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::resource('/customers', 'ApiUserController');
-Route::get('/customers/{id}/rentals', 'ApiUserController@rentals');
-Route::resource('/movies', 'ApiMovieController');
-//returns the data for individuals who rented a single movie
-Route::get('/movies/{id}/rentals', 'ApiMovieController@RentedBy');
+
+Route::group(['middleware' => 'cors'], function () {
+    Route::resource('/customers', 'ApiUserController');
+    Route::get('/customers/{id}/rentals', 'ApiUserController@rentals');
+    Route::resource('/movies', 'ApiMovieController');
+    //returns the data for individuals who rented a single movie
+    Route::get('/movies/{id}/rentals', 'ApiMovieController@RentedBy');
+});
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-
