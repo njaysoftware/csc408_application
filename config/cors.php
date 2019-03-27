006ca5b92889a24c2d3813972fd38f0718b93f90
EC2 Default User <ec2-user@ip-172-31-22-31.us-east-2.compute.internal>
2019-03-27 20:46:45 +0000

    fixed cors issue

--- config/cors.php
+++ config/cors.php
@@ -13,7 +13,7 @@ return [
     */
 
     'supportsCredentials' => false,
-    'allowedOrigins' => ['localhost:8080'],
+    'allowedOrigins' => ['*'],
     'allowedOriginsPatterns' => [],
     'allowedHeaders' => ['*'],
     'allowedMethods' => ['*'],
